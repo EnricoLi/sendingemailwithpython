4f6093c4a240f221e0e5bfd837f7af88a2b5406d
EnricoLi <enricolima.eng@gmail.com>
2022-03-23 18:57:17 -0300

    version 1.0a

--- sendingemailwithpython/views.py
+++ sendingemailwithpython/views.py
@@ -2,6 +2,7 @@ from email import message
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.mail import send_mail
+import re
 
 def index(request):
     if request.method == 'POST':
@@ -10,18 +11,23 @@ def index(request):
         subject = request.POST.get('subject')
         message = request.POST.get('message')
 
-        data = {
-            'name': name,
-            'email': email,
-            'subject': subject,
-            'message': message
-        }
-        message = '''
-        New message: {}
+        valid_email = '^\w+(?:[.\+!%]\w+)*@\w+(?:[.\-]\w+)+$'
 
-        From:{}
-        '''.format(data['message'], data['email'])
-        send_mail(data['subject'], message, '', 
-        ['example@mail.com'])
+        if (re.search(valid_email, email)):
+            data = {
+                'name': name,
+                'email': email,
+                'subject': subject,
+                'message': message
+            }
+            message = '''
+            New message: {}
+
+            From:{}
+            '''.format(data['message'], data['email'])
+            send_mail(data['subject'], message, '', 
+            ['example@mail.com'])
+        else:
+            return render(request, 'sendingemailwithpython/invalid.html', {})
 
     return render(request, 'sendingemailwithpython/index.html', {})
